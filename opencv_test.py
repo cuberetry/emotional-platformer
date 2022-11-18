03fb2500e7fdb7dd4218323788c39225fdd1762c
Tanapol Dangsakul <tanapol.dgsk@gmail.com>
2022-11-18 02:19:14 +0100

    Create simple openCV example

---
+++ opencv_test.py
@@ -0,0 +1,17 @@
+import cv2
+
+# Init a screen obj
+vid_screen = cv2.VideoCapture(0)
+
+while True:
+    # Capture
+    ret, frame = vid_screen.read()
+    # Display
+    cv2.imshow('OpenCV Window', frame)
+    # Stop if any key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release and destroy the screen obj
+vid_screen.release()
+cv2.destroyAllWindows()
