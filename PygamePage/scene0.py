40c3e8c436070898ce809e2ce54e380bf713e982
Tanapol Dangsakul <tanapol.dgsk@gmail.com>
2022-11-19 23:36:29 +0100

    Setup Pygame scene as a class. Remove the main openCV scene and change the output to a console log. Add the Pygame to the main driver code along with openCV driver.

---
+++ PygamePage/scene0.py
@@ -0,0 +1,31 @@
+import sys
+from InstanceModel.spr_platform import *
+from InstanceModel.spr_player import *
+import GlobalVariable.game_setting as gb_setting
+import GlobalVariable.sprite_group as gb_spr
+
+
+class MainPygameWindow:
+    def __init__(self):
+        pygame.init()
+        self.FramePerSec = pygame.time.Clock()
+
+        self.display_surface = pygame.display.set_mode((gb_setting.WIDTH, gb_setting.HEIGHT))
+        pygame.display.set_caption("Emotional Platformer")
+
+        self.P1 = Player()
+        self.platform_1 = Platform(gb_setting.WIDTH, gb_setting.HEIGHT)
+
+    def main_loop(self):
+        self.P1.move()
+        self.P1.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        self.display_surface.fill((0, 0, 0))
+
+        for entity in gb_spr.all_sprites:
+            self.display_surface.blit(entity.surf, entity.rect)
+        pygame.display.update()
+        self.FramePerSec.tick(gb_setting.FPS)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import OpenCVPage.emotion_detector_model as cv
+import PygamePage.scene0 as pg
 
 if __name__ == '__main__':
     main_emotion_detector = cv.EmotionDetector()
+    main_pygame_window = pg.MainPygameWindow()
 
     while True:
         main_emotion_detector.main_loop()
+        print("Current emotion:", main_emotion_detector.label)
+        main_pygame_window.main_loop()
