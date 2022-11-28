2d6d0720d27bd2716187b19d1bcbb5e4e6ca7e54
Tanapol Dangsakul <tanapol.dgsk@gmail.com>
2022-11-29 00:44:19 +0100

    Fixed all issues in PR

--- InstanceModel/spr_player.py
+++ InstanceModel/spr_player.py
@@ -1,7 +1,6 @@
 import pygame
 from pygame.locals import *
 import GlobalVariable.game_var as gb_var
-import GlobalVariable.game_setting as gb_setting
 import GlobalVariable.sprite_group as gb_spr
 from InstanceModel import spr_pause_menu as pause_menu
 
@@ -9,7 +8,7 @@ vec = pygame.math.Vector2
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, acc_rate=gb_var.ACCELERATION):
+    def __init__(self):
         super().__init__()
         self.emotion_state = gb_var.EMOTION
         gb_spr.all_sprites.add(self)
@@ -24,10 +23,12 @@ class Player(pygame.sprite.Sprite):
 
         # Physic setup
         self.direction = vec(0, 0)
-        self.speed = 5
+        self.speed = 8
         self.gravity = 0.8
         self.jump_speed = -16
         self.jumped = False
+        self.acc = gb_var.ACCELERATION
+        self.friction = gb_var.FRICTION
 
     # Simple movement
     def move(self):
@@ -48,7 +49,7 @@ class Player(pygame.sprite.Sprite):
             self.direction.y = self.jump_speed
 
         # Movement handling
-        self.rect.x += self.direction.x * self.speed
+        self.rect.x += self.direction.x * (self.speed + 0.5 * self.acc * self.friction)
         hit_platform = gb_spr.env_sprites
         for entity in hit_platform.sprites():
             if entity.rect.colliderect(self.rect):
