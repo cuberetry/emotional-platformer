2e24f4d43c1b2c5a5332bc4fbace7c92495391c3
Tanapol Dangsakul <tanapol.dgsk@gmail.com>
2022-12-03 11:24:38 +0100

    Added enemy model Added player and enemy collision Added stage update logic for enemy sprites

---
+++ InstanceModel/spr_enemy.py
@@ -0,0 +1,42 @@
+import pygame
+import GlobalVariable.game_var as gb_var
+import GlobalVariable.sprite_group as gb_spr
+
+vec = pygame.math.Vector2
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        gb_spr.all_sprites.add(self)
+        gb_spr.enemy_sprites.add(self)
+
+        # Rendering setup
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((0, 150, 75))
+        self.rect = self.surf.get_rect(center=(10, 420))
+        self.left_border, self.right_border = -50, 1450
+
+        # Physic setup
+        self.direction = vec(1, 0)
+        self.speed = 1
+        self.gravity = 0.8
+
+    # Update instance status
+    def update(self):
+        # Pausing
+        if gb_var.IS_PAUSING:
+            return
+
+        # Movement and platform collision
+        self.rect.x += self.direction.x * self.speed
+        hit_platform = gb_spr.env_sprites
+        for entity in hit_platform.sprites():
+            if self.rect.colliderect(entity):
+                self.direction.x = -self.direction.x
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+        for entity in hit_platform.sprites():
+            if self.rect.colliderect(entity):
+                self.rect.bottom = entity.rect.top
+                self.direction.y = 0
