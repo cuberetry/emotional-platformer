66786b5397e22eb501458973cdf7e8fc462e10f0
Tanapol Dangsakul <tanapol.dgsk@gmail.com>
2023-02-08 00:23:38 +0100

    Added ice melt and player death particles

--- InstanceModel/Environment/spr_particle_emitter.py
+++ InstanceModel/Environment/spr_particle_emitter.py
@@ -14,18 +14,42 @@ class ParticleEmitter:
                 p[0][0] += p[2][0]
                 p[0][1] += p[2][1]
                 p[1] -= 0.2
-                if p[4] == 'Rect':
-                    pygame.draw.rect(gb_var.SURFACE, p[3],
-                                     p[0], int(p[1]))
-                elif p[4] == 'Circle':
+                if p[4] == 'Circle':
                     pygame.draw.circle(gb_var.SURFACE, p[3],
                                        p[0], int(p[1]))
 
-    def add_particles(self, x, y, amount=1, color='White', shape='Circle'):
-        for _ in range(amount):
+    def add_particles(self, x, y, amount=1, ptype='Jump'):
+        c_list = ['blue', 'purple', 'green', 'orange',
+                  'red', 'yellow', 'white']
+        if ptype == 'Jump':
             radius = r.randint(5, 7)
             dir_x = r.randint(-5, 5)
             dir_y = r.randint(0, 5)
+            color = 'White'
+            shape = 'Circle'
+        elif ptype == 'Death':
+            radius = 4
+            dir_x = r.randint(-5, 5)
+            dir_y = r.randint(-5, 5)
+            color = 'White'
+            shape = 'Circle'
+        elif ptype == 'Ice':
+            radius = 4
+            dir_x = r.randint(-5, 5)
+            dir_y = r.randint(-5, 5)
+            color = 'skyblue'
+            shape = 'Circle'
+        elif ptype == 'Fire':
+            radius = 4
+            dir_x = -1
+            dir_y = r.randint(-5, 5)
+            shape = 'Circle'
+            color = 'darkorange'
+        else:
+            radius, dir_x, dir_y = 1, 1, 1
+            color = "White"
+            shape = "Circle"
+        for p in range(amount):
             particle_circle = [[x, y], radius, [dir_x, dir_y], color, shape]
             self.particles.append(particle_circle)
 
